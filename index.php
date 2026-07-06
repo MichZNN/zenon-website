4acaf81351704364a305d8d61278e8a3cdaa34d5
MichZNN <michznn@turmin.com>
2026-07-06 15:37:27 +0200

    fix: adjust navbar spacing and homepage logo scale

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ require_once("wznn_data.php");
         </li>
 
         <li class="nav-item">
-          <span class="nav-link text-body-secondary" style="font-size: 0.875rem;">
+          <span class="nav-link navbar-price">
           <?php
           if(isset($cache)) {
             echo "ZNN &dollar;" . $cache['wznn_weth']['data']['priceUsd'] . "&nbsp;";
